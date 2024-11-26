2b870df5db046a4c5fd776669fe04d48265eb94b
MD. Jahidul Islam Jony <jahiduljony60@gmail.com>
2024-11-26 19:48:02 +0600

    2938. Separate Black and White Balls.py

---
+++ 2938. Separate Black and White Balls.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        res = left = 0
+        for right in range(len(s)):
+            if s[right] == "0":
+                res += right - left
+                left += 1
+        return res
