c384d3a9af56eba7733e25cc2a37afc60df0ffb5
MD. Jahidul Islam Jony <jahiduljony60@gmail.com>
2025-08-25 03:54:38 +0600

    Add files via upload

---
+++ 976. Largest Perimeter Triangle.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int largestPerimeter(vector<int>& nums)
+    {
+        if(nums.size()<3) return 0;
+
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        for(int i=n-1; i>1; i--)
+        {
+            if(nums[i-1]+nums[i-2]>nums[i])
+            {
+                return nums[i]+nums[i-1]+nums[i-2];
+            }
+        }
+        return 0;
+    }
+};
