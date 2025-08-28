ff5a94a9de87f849c8c07a369c66e37f804e8c8e
MD. Jahidul Islam Jony <jahiduljony60@gmail.com>
2025-08-28 23:36:06 +0600

    2864. Maximum Odd Binary Number.cpp

--- 2864. Maximum Odd Binary Number.cpp
+++ 2864. Maximum Odd Binary Number.cpp
@@ -1,1 +1,22 @@
+class Solution
+{
+public:
+    string maximumOddBinaryNumber(string s)
+    {
+        int n = s.length(), cnt0 = 0, cnt1 = -1;
+        \
+
+        for(int i = 0; i < n; ++i)
+        {
+            if(s[i] == '0')
+            {
+                ++cnt0;
+            }
+            else 
+                ++cnt1;
+        }
+        return string(cnt1, '1') + string(cnt0, '0') + '1';
+    }
+};
+
 
