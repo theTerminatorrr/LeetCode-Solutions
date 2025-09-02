a61c334154c67671f68323107431515e79f11850
MD. Jahidul Islam Jony <jahiduljony60@gmail.com>
2025-09-03 00:25:01 +0600

    Add files via upload

---
+++ 509. Fibonacci Number.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    vector<int> memo;
+
+    int fib(int n)
+    {
+        memo.assign(n + 1, -1);
+
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        if (memo[n] != -1) return memo[n];
+
+        memo[n] = fib(n - 1) + fib(n - 2);
+
+        return memo[n];
+    }
+};
