01d5f817066a1d30fe044741767a00c22e3691e2
MD. Jahidul Islam Jony <jahiduljony60@gmail.com>
2025-09-02 23:26:10 +0600

    Add files via upload

--- 70. Climbing Stairs.cpp
+++ 70. Climbing Stairs.cpp
@@ -1,14 +1,21 @@
-class Solution {
+class Solution
+{
 public:
-    int climbStairs(int n) {
-        int prev = 1, prev_prev = 1;
-
-        for (int i = 1; i < n; i++) {
-            int temp = prev;
-            prev += prev_prev;
-            prev_prev = temp;
+    int climbStairs(int n)
+    {
+        if (n == 0 || n == 1)
+        {
+            return 1;
         }
 
-        return prev;
+        vector <int> dp(n+1);
+        dp[0] = dp[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
 };
